329ac78b51fd539f0409bd07a66c628a8572fbd0
Your Name <you@example.com>
2026-04-10 00:53:42 +0200

    fixed the image size

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,9 +2,15 @@ import { BootstrapContext, bootstrapApplication } from '@angular/platform-browse
 import { App } from './app/app';
 import config from './app/app.config.server';
 import 'zone.js/node';
-
+import { provideServerRendering } from '@angular/platform-server';
 
 const bootstrap = (context: BootstrapContext) =>
-    bootstrapApplication(App, config, context);
+  bootstrapApplication(App, {
+    ...config,
+    providers: [
+      ...(config.providers ?? []),
+      provideServerRendering()
+    ]
+  }, context);
 
 export default bootstrap;
